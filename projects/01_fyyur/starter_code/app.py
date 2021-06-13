e199c70036cf46576a7f3a3f67789f0f15484659
Alya <AlyaAlotibi@outlook.sa>
2021-06-13 03:24:14 +0300

    edit show table

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -74,8 +74,6 @@ class Artist(db.Model):
 class Show(db.Model):
   __tablename__ = 'Show'
   id=db.Column(db.Integer,primary_key=True)
-  name=db.Column(db.String)
-  city = db.Column(db.String(120))
   start_time=db.Column(db.DateTime)
   venue_id=db.Column(db.Integer,db.ForeignKey('Venue.id'))
   artist_id=db.Column(db.Integer,db.ForeignKey('Artist.id'))
